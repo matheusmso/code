88a49237ba845090b5b75ea2349a15e910288f64
Matheus Oliveira <matheusmellosoliveira@gmail.com>
2017-09-30 13:18:57 -0300

    fixed

--- codeforces/272/c.cpp
+++ codeforces/272/c.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long lint;
 const int N = 1e5+7;
-int n, m, v[N], w1, h1, w2, h2;
+int n, m;
+lint v[N], w1, h1, w2, h2;
 
 int main() {
     scanf(" %d", &n);
     for (int i = 1; i <= n; i++)
         scanf(" %d", v+i);
     scanf(" %d", &m);
-    int p = 0;
+    lint p = 0;
     for (int i = 0; i < m; i++) {
-        scanf(" %d %d", &w2, &h2);
+        scanf(" %lld %lld", &w2, &h2);
         p = max(p+h1, v[w2]);
-        printf("%d\n", p);
+        cout << p << endl;
         w1 = w2;
         h1 = h2;
     }
