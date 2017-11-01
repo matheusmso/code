5a30bb1169eae34c40eeb278cc83abd2d0f077f1
Matheus Oliveira <matheusmellosoliveira@gmail.com>
2017-11-01 15:57:54 -0200

    spojbr troco13
    much better with help

--- spojbr/troco13.cpp
+++ spojbr/troco13.cpp
@@ -9,28 +9,19 @@ const int INF = 0x3f3f3f3f;
 const lint LINF = 0x3f3f3f3f3f3f3f3f;
 
 const int N = 1e5+7;
-const int M = 1e3+7;
-int v, m, c[M];
-int dp[N][M];
-map<pair<int, int>, int> mp;
-
-bool solve(int k, int i) {
-    if (k == 0) return true;
-    if (i == m || k < 0) return false;
-    if (dp[k][i] != -1)
-        return dp[k][i];
-    if (solve(k, i+1) || solve(k-c[i], i+1))
-        return dp[k][i] = true;
-    return dp[k][i] = false;;
-}
+int v, m, dp[N];
 
 int main() {
     scanf(" %d %d", &v, &m);
-    memset(dp, -1, sizeof(dp));
-    for (int i = 0; i < m; i++)
-        scanf(" %d", c+i);
-    sort(c, c+m);
-    printf("%c\n", solve(v, 0) ? 'S' : 'N');
+    dp[0] = 1;
+    for (int i = 0; i < m; i++) {
+        int x;
+        scanf(" %d", &x);
+        for (int j = v; j >= x; j--)
+            if (dp[j-x])
+                dp[j] = 1;
+    }
+    printf("%c\n", dp[v] ? 'S' : 'N');
     return 0;
 }
 
