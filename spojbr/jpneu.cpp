3e160e84aae6d700dbf14f12d1b87697a4fba50a
Matheus Oliveira <matheusmellosoliveira@gmail.com>
2017-11-11 23:01:14 -0200

    spojbr jpneu

---
+++ spojbr/jpneu.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(args...) fprintf(stderr,args)
+
+typedef long long lint;
+
+const int INF = 0x3f3f3f3f;
+const lint LINF = 0x3f3f3f3f3f3f3f3f;
+
+int a, b;
+
+int main() {
+    scanf(" %d %d", &a, &b);
+    printf("%d\n", a-b);
+    return 0;
+}
+
+
