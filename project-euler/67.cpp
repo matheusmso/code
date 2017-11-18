433978f3e693bfdedd38ffe184746771c4cba8b0
Matheus Oliveira <matheusmellosoliveira@gmail.com>
2017-11-18 04:59:33 -0200

    project euler 67

---
+++ project-euler/67.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(args...) fprintf(stderr,args)
+
+typedef long long lint;
+
+const int INF = 0x3f3f3f3f;
+const lint LINF = 0x3f3f3f3f3f3f3f3f;
+
+const int N = 200;
+int g[N][N], dp[N][N];
+char s[N];
+
+int solve(int i, int j) {
+    if (i < j) return -INF;
+    if (i == 100) return 0;
+    if (dp[i][j] != -1) return dp[i][j];
+    int ans = max(solve(i+1, j), solve(i+1, j+1));
+    return dp[i][j] = ans+g[i][j];
+}
+
+int main() {
+    for (int i = 0; i < 100; i++)
+        for (int j = 0; j <= i; j++) {
+            scanf(" %s", s);
+            g[i][j] = atoi(s);
+        }
+    memset(dp, -1, sizeof(dp));
+    printf("%d\n", solve(0, 0));
+    return 0;
+}
+
+
