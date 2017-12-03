4bff7fdaca31f5d303977cfeced8483865f7b568
Matheus Oliveira <matheusmellosoliveira@gmail.com>
2017-12-03 06:52:27 -0200

    testing user

---
+++ t.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(args...) fprintf(stderr,args)
+
+typedef long long lint;
+
+const int INF = 0x3f3f3f3f;
+const lint LINF = 0x3f3f3f3f3f3f3f3f;
+
+int main() {
+    printf("test");
+    return 0;
+}
+
+
